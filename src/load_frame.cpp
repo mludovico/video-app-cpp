3a2ce55f34f01d94899e678e9c98d0721f740b7e
marcelo <marcelo@hexplore.com>
2021-01-31 18:05:38 -0300

    Added scale to convert YUV to RGBA and render to openGL surface

--- src/load_frame.cpp
+++ src/load_frame.cpp
@@ -1,23 +1,24 @@
 extern "C"{
     #include <libavcodec/avcodec.h>
     #include <libavformat/avformat.h>
+    #include <libswscale/swscale.h>
     #include <inttypes.h>
 }
 
 #include "helpers.hpp"
 
-bool load_frame(const char* filename, int* width, int* height, unsigned char** data) {
+bool load_frame(const char* filename, int* width_out, int* height_out, unsigned char** data_out) {
     
     // Open file using libavformat
     AVFormatContext* av_format_context = avformat_alloc_context();
     if (!av_format_context) {
-        printf("Could not create avformat context.\n");
-        return false;
+        exitWithError(false, "Could not create avformat context.\n");
     }
 
-    if(avformat_open_input(&av_format_context, filename, NULL, NULL) != 0) {
-        printf("Could not open video file");
-        return false;
+    int response = avformat_open_input(&av_format_context, filename, NULL, NULL);
+    if(response != 0) {
+        printf("Could not open video file\n");
+        exitWithError(false, av_err2str(response));
     }
 
     //Find video stream index
@@ -66,7 +67,6 @@ bool load_frame(const char* filename, int* width, int* height, unsigned char** d
         exitWithError(false, "Could not allocate packet");
     }
 
-    int response;
     while (av_read_frame(av_format_context, av_packet) >= 0) {
         if(av_packet->stream_index != video_stream_index) {
             continue;
@@ -83,12 +83,39 @@ bool load_frame(const char* filename, int* width, int* height, unsigned char** d
             printf("Failed to decode packet\n");
             exitWithError(false, av_err2str(response));
         }
+        av_packet_unref(av_packet);
+        break;
     }
 
+    SwsContext* sws_scaler_context = sws_getContext(
+        av_frame->width,
+        av_frame->height,
+        av_codec_context->pix_fmt,
+        av_frame->width,
+        av_frame->height,
+        AV_PIX_FMT_RGB0,
+        SWS_BILINEAR,
+        NULL, NULL, NULL
+    );
+
+    if (!sws_scaler_context) {
+        exitWithError(false, "Could not initialize sw scaler\n");
+    }
+    uint8_t* data = new uint8_t[av_frame->width * av_frame->height * 4];
+    uint8_t* dest[4] = { data, NULL, NULL, NULL };
+    int dest_linesize[4] = { av_frame->width * 4, 0, 0, 0 };
+    sws_scale(sws_scaler_context, av_frame->data, av_frame->linesize, 0, av_frame->height, dest, dest_linesize);
+    sws_freeContext(sws_scaler_context);
+
+    *width_out = av_frame->width;
+    *height_out = av_frame->height;
+    *data_out = data;
 
     avformat_close_input(&av_format_context);
     avformat_free_context(av_format_context);
     avcodec_free_context(&av_codec_context);
+    av_packet_free(&av_packet);
+    avcodec_free_context(&av_codec_context);
 
-    return false;
+    return true;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,31 +5,34 @@
 bool load_frame(const char* filename, int* width, int* height, unsigned char** data);
 
 int main(int argc, char** argv) {
+
+  char* filename = argv[1];
   GLFWwindow* window;
 
   if(!glfwInit()) exitWithError(1, "Could not initialize GLFW");
 
-  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  window = glfwCreateWindow(1280, 720, "Hello World", NULL, NULL);
   if(!window) exitWithError(1, "Could not open window");
 
-  unsigned char* data = new unsigned char[100 * 100 *3];
+  unsigned char* frame_data;
   int frame_height, frame_width;
   
-  if(!load_frame("/Users/marcelo/Desktop/video_sample.mp4", &frame_width, &frame_height, &data))
+  if(!load_frame(filename, &frame_width, &frame_height, &frame_data))
     exitWithError(1, "Could not load frame.\n");
 
   glfwMakeContextCurrent(window);
 
+  printf("Frame height: %i, frame width: %i\n", frame_height, frame_width);
   GLuint tex_handle;
   glGenTextures(1, &tex_handle);
   glBindTexture(GL_TEXTURE_2D, tex_handle);
   glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, frame_width, frame_height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, frame_width, frame_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, frame_data);
 
   while(!glfwWindowShouldClose(window)){
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -39,17 +42,17 @@ int main(int argc, char** argv) {
     glfwGetFramebufferSize(window, &window_width, &window_height);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(0, window_width, 0, window_height, -1, 1);
+    glOrtho(0, window_width, 0, frame_height, -1, 1);
     glMatrixMode(GL_MODELVIEW);
 
     // Render whatever buffer on screen
     glEnable(GL_TEXTURE_2D);
     glBindTexture(GL_TEXTURE_2D, tex_handle);
     glBegin(GL_QUADS);
-      glTexCoord2d(0, 0); glVertex2i(200, 200);
-      glTexCoord2d(1, 0); glVertex2i(200 + frame_width, 200);
-      glTexCoord2d(1, 1); glVertex2i(200 + frame_width, 200 + frame_height);
-      glTexCoord2d(0, 0); glVertex2i(200, 200 + frame_height);
+      glTexCoord2d(0, 0); glVertex2i(0, frame_height);
+      glTexCoord2d(1, 0); glVertex2i(frame_width, frame_height);
+      glTexCoord2d(1, 1); glVertex2i(frame_width, 0);
+      glTexCoord2d(0, 1); glVertex2i(0, 0);
     glEnd();
     glDisable(GL_TEXTURE_2D);
 
